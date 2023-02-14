c50346ceadd1e614daf7d263f9ab2515f61f21b9
Игорь Новоселов <barszx23@gmail.com>
2023-02-14 17:37:37 +0400

    change cmd

--- PowerCollections/build_and_publish_nuget.cmd
+++ PowerCollections/build_and_publish_nuget.cmd
@@ -1,3 +1,2 @@
 dotnet pack
-pushd PowerCollections\bin\Debug
-dotnet nuget push "*.nupkg" -k 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 -s https://nuget.pkg.github.com/Novoselov-Igor/index.json --skip-duplicate
+dotnet nuget push "PowerCollections\bin\Debug\*.nupkg" -k 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 -s https://nuget.pkg.github.com/Novoselov-Igor/index.json --skip-duplicate
